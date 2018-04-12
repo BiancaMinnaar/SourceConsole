b2261e4e338dd36e0c7c9db4246fbe29cf4616b1
Bianca Minnaar <bminnaar@gmail.com>
2018-04-12 15:15:34 +0200

    Add GeneratorTemplate for partial template classes

---
+++ Templates/Scafholding/GeneratorTemplates/TemplatePartialTemplatePartial.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SourceConsole.Templates.Scafholding.GeneratorTemplates
+{
+    partial class TemplatePartialTemplatePartial
+    {
+        public TemplatePartialTemplatePartial()
+        {
+        }
+    }
+}
