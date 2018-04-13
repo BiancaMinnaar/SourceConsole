3d5958182263e73c66791f8174a336a09b8303cb
Bianca Minnaar <bminnaar@gmail.com>
2018-04-13 11:00:45 +0200

    Abstract Template Logic to extensions

--- Templates/Scafholding/GeneratorTemplates/TemplatePartialTemplatePartial.cs
+++ Templates/Scafholding/GeneratorTemplates/TemplatePartialTemplatePartial.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using SourceConsole.Templates.DataModel;
+﻿using SourceConsole.Templates.DataModel;
 using SourceConsole.Templates.PartialClasses;
 
 namespace SourceConsole.Templates.Scafholding.GeneratorTemplates
 {
+    public static class Extentions
+    {
+        public static string GetFileName<T>(this T template) where T:ITemplate
+        {
+            var repo = new SourceFileMapRepository<T>();
+            template.DataModel._Template = new FileModel()
+            {
+                CodeName = template.DataModel.Template,
+                Extension = repo.GetSourceExtension(template),
+                ProjectFilePath = repo.GetSourcePath(template)
+            };
+            return template.DataModel._Template.FileName;
+        }
+
+        public static string GetFullProjectFileName<T>(this T template) where T : ITemplate
+        {
+            return template.DataModel._Template.FullProjectFileName;
+        }
+    }
     partial class TemplatePartialTemplate : ITemplate
     {
-        public string FullProjectFileName => DataModel._Template.FullProjectFileName;
+        public string FullProjectFileName => this.GetFullProjectFileName();
 
         public SourceEnum TemplateEnum => SourceEnum.Generator;
 
@@ -14,16 +32,14 @@ namespace SourceConsole.Templates.Scafholding.GeneratorTemplates
 
         public TemplateDataModel DataModel { get; set; }
 
+        public TemplatePartialTemplate(TemplateDataModel dataModel)
+        {
+            DataModel = dataModel;
+        }
+
         public string GetFileName()
         {
-            var repo = new SourceFileMapRepository<TemplatePartialTemplate>();
-            DataModel._Template = new FileModel()
-            {
-                CodeName = DataModel.Template,
-                Extension = repo.GetSourceExtension(this),
-                ProjectFilePath = repo.GetSourcePath(this)
-            };
-            return DataModel._Template.FileName;
+            return this.GetFileName();
         }
     }
 }
