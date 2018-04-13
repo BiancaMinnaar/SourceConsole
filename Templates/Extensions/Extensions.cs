b8d7ca942e8fe9f2e97c2e22126d82a8530c9724
Bianca Minnaar <bminnaar@gmail.com>
2018-04-13 11:07:17 +0200

    Migrate extensions out of partial class definition.

---
+++ Templates/Extensions/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SourceConsole.Templates.DataModel;
+
+namespace SourceConsole.Templates.Extensions
+{
+    public static class Extentions
+    {
+        public static string GetFileName<T>(this T template) where T : ITemplate
+        {
+            var repo = new SourceFileMapRepository<T>();
+            template.DataModel._Template = new FileModel()
+            {
+                CodeName = template.DataModel.Template,
+                Extension = repo.GetSourceExtension(template),
+                ProjectFilePath = repo.GetSourcePath(template)
+            };
+            return template.DataModel._Template.FileName;
+        }
+
+        public static string GetFullProjectFileName<T>(this T template) where T : ITemplate
+        {
+            return template.DataModel._Template.FullProjectFileName;
+        }
+    }
+}
--- Templates/Scafholding/GeneratorTemplates/TemplatePartialTemplatePartial.cs
+++ Templates/Scafholding/GeneratorTemplates/TemplatePartialTemplatePartial.cs
@@ -1,27 +1,9 @@
 ﻿using SourceConsole.Templates.DataModel;
+using SourceConsole.Templates.Extensions;
 using SourceConsole.Templates.PartialClasses;
 
 namespace SourceConsole.Templates.Scafholding.GeneratorTemplates
 {
-    public static class Extentions
-    {
-        public static string GetFileName<T>(this T template) where T:ITemplate
-        {
-            var repo = new SourceFileMapRepository<T>();
-            template.DataModel._Template = new FileModel()
-            {
-                CodeName = template.DataModel.Template,
-                Extension = repo.GetSourceExtension(template),
-                ProjectFilePath = repo.GetSourcePath(template)
-            };
-            return template.DataModel._Template.FileName;
-        }
-
-        public static string GetFullProjectFileName<T>(this T template) where T : ITemplate
-        {
-            return template.DataModel._Template.FullProjectFileName;
-        }
-    }
     partial class TemplatePartialTemplate : ITemplate
     {
         public string FullProjectFileName => this.GetFullProjectFileName();
