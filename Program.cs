656dd0dd7301c2c5000302b52041fd0851ebe91f
Bianca Minnaar <bminnaar@gmail.com>
2018-04-19 19:07:02 +0200

    Move generation signals to Repo

--- Program.cs
+++ Program.cs
@@ -11,32 +11,8 @@ namespace SourceConsole
     {
         public static void Main(string[] args)
         {
-            IProjectReaderRepository readerRepo = new ProjectReaderRepository(new FileService());
-            IGenerationReposetory<GroupTemplateDataModel> repo = 
-                new GenerationReposetory<GroupTemplateDataModel>(new FileService(), new SimpleCSharpProjectFactory(readerRepo));
-            var screenData = repo.GetBaseDataModel(() =>
-                {
-                    Console.Write("Template Name:");
-                    return Console.ReadLine();
-            });
-            repo.WriteTemplateToFile(screenData, 
-                                     new SourceFileMapRepository<SourceConsole.Templates.Scafholding.NormalTemplates.ViewModelTemplate, GroupTemplateDataModel>(
-                                         readerRepo));
-            //repo.WriteTemplateToFile(new SourceConsole.Templates.NormalTemplates.ViewTemplate(screenData));
-            //repo.WriteTemplateToFile(new SourceConsole.Templates.NormalTemplates.ViewCodeBehindTemplate(screenData));
-            //repo.WriteTemplateToFile(new SourceConsole.Templates.NormalTemplates.ViewControllerInterfaceTemplate(screenData));
-            //repo.WriteTemplateToFile(new SourceConsole.Templates.NormalTemplates.ViewControllerTemplate(screenData));
-            //repo.WriteTemplateToFile(new SourceConsole.Templates.NormalTemplates.RepositoryInterfaceTemplate(screenData));
-            //repo.WriteTemplateToFile(new SourceConsole.Templates.NormalTemplates.RepositoryTemplate(screenData));
-            //repo.WriteTemplateToFile(new SourceConsole.Templates.NormalTemplates.ServiceInterfaceTemplate(screenData));
-            //repo.WriteTemplateToFile(new SourceConsole.Templates.NormalTemplates.ServiceTemplate(screenData));
-
-            //repo.WriteTemplateToFile<SourceConsole.Templates.Framework.ProjectBaseContentPageTemplate,GroupTemplateDataModel>(new SourceConsole.Templates.Framework.ProjectBaseContentPageTemplate(screenData));
-            //var screenData = new TemplateDataModel()
-            //{
-            //    ProjectName = ""
-            //};
-            //repo.WriteTemplateToFile(new Templates.Swift.Swift_RepositoryTemplate(screenData), screenData);
+            var MainRepo = new XamarinFormsRepository();
+            MainRepo.GenerateXamarinScreen();
         }
     }
 }
---
+++ Repository/Implementation/XamarinFormsRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CorePCL.Generation.Repository;
+using CorePCL.Generation.Repository.Implementation;
+using SourceConsole.Factory;
+using SourceConsole.Templates.DataModel;
+using SourceConsole.Templates.Scafholding.NormalTemplates;
+
+namespace SourceConsole.Repository.Implementation
+{
+    public class XamarinFormsRepository
+    {
+        public XamarinFormsRepository()
+        {
+        }
+
+        public void GenerateXamarinScreen()
+        {
+            IProjectReaderRepository readerRepo = new ProjectReaderRepository(new FileService());
+            IGenerationReposetory<GroupTemplateDataModel> repo =
+                new GenerationReposetory<GroupTemplateDataModel>(new FileService(), new SimpleCSharpProjectFactory(readerRepo));
+            var screenData = repo.GetBaseDataModel(() =>
+            {
+                Console.Write("Template Name:");
+                return Console.ReadLine();
+            });
+            repo.WriteTemplateToFile(screenData, new SourceFileMapRepository<ViewModelTemplate, GroupTemplateDataModel>(readerRepo));
+            repo.WriteTemplateToFile(screenData, new SourceFileMapRepository<ViewTemplate, GroupTemplateDataModel>(readerRepo));
+            repo.WriteTemplateToFile(screenData, new SourceFileMapRepository<ViewCodeBehindTemplate, GroupTemplateDataModel>(readerRepo));
+            repo.WriteTemplateToFile(screenData, new SourceFileMapRepository<ViewControllerInterfaceTemplate, GroupTemplateDataModel>(readerRepo));
+            repo.WriteTemplateToFile(screenData, new SourceFileMapRepository<ViewControllerTemplate, GroupTemplateDataModel>(readerRepo));
+            repo.WriteTemplateToFile(screenData, new SourceFileMapRepository<RepositoryInterfaceTemplate, GroupTemplateDataModel>(readerRepo));
+            repo.WriteTemplateToFile(screenData, new SourceFileMapRepository<RepositoryTemplate, GroupTemplateDataModel>(readerRepo));
+            repo.WriteTemplateToFile(screenData, new SourceFileMapRepository<ServiceInterfaceTemplate, GroupTemplateDataModel>(readerRepo));
+            repo.WriteTemplateToFile(screenData, new SourceFileMapRepository<ServiceTemplate, GroupTemplateDataModel>(readerRepo));
+        }
+    }
+}
