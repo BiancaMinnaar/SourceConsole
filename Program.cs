bbfa3e50e223bbada52b7c77bf688ece1a15fd7a
Bianca Minnaar <bminnaar@gmail.com>
2018-05-06 11:07:03 +0200

    Configure for Pre Gen

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ namespace SourceConsole
         public static void Main(string[] args)
         {
             var readerRepo = new ProjectReaderRepository(new FileService());
-            var MainRepo = new XamarinFormsScreenGeneratorRepository(
-            //var MainRepo = new BonsaiFrameworkRepository(
+            //var MainRepo = new XamarinFormsScreenGeneratorRepository(
+            var MainRepo = new BonsaiFrameworkRepository(
                 new FileService(), 
                 new SimpleCSharpProjectFactory(readerRepo),
                 readerRepo);
