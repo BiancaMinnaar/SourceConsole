90f76006f1cd7bb57f320ef8270def6225c6e22b
Bianca Minnaar <bminnaar@gmail.com>
2018-06-05 22:40:03 +0200

    Initiate Upgrade 1.1.1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Templater.Factory;
 using Templater.Repository.Implementation;
 using Templater.Service.Implementation;
+using TemplaterBonsai.Repository.Implementation;
 
 namespace SourceConsole
 {
@@ -9,8 +10,9 @@ namespace SourceConsole
         public static void Main(string[] args)
         {
             var readerRepo = new ProjectReaderRepository(new FileService());
+			//var MainRepo = new BonsaiFrameworkRepository(
             //var MainRepo = new XamarinFormsScreenGeneratorRepository(
-            var MainRepo = new BonsaiFrameworkRepository(
+            var MainRepo = new BonsaiUpgrade1_1_1(
                 new FileService(), 
                 new SimpleCSharpProjectFactory(readerRepo),
                 readerRepo);
