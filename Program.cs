f1119ac9999156246119cb9b5e5bfbbd68c3d9a2
Bianca Minnaar <bminnaar@gmail.com>
2018-06-08 13:59:39 +0200

    Reset Default Config.

--- Program.cs
+++ Program.cs
@@ -10,9 +10,8 @@ namespace SourceConsole
         public static void Main(string[] args)
         {
             var readerRepo = new ProjectReaderRepository(new FileService());
-			//var MainRepo = new BonsaiFrameworkRepository(
+			var MainRepo = new BonsaiFrameworkRepository(
             //var MainRepo = new XamarinFormsScreenGeneratorRepository(
-            var MainRepo = new BonsaiUpgrade1_1_1(
                 new FileService(), 
                 new SimpleCSharpProjectFactory(readerRepo),
                 readerRepo);
