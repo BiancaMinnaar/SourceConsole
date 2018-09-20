a35ca1f8b19ee26aa75c52f6be863db3a62d6814
Bianca Minnaar <bminnaar@gmail.com>
2018-09-20 11:41:39 +0200

    no message

--- Program.cs
+++ Program.cs
@@ -1,20 +1,64 @@
-﻿using Templater.Factory;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MobileBonsai.Generation.Repository.Implementation;
+using Templater.Factory;
 using Templater.Repository.Implementation;
 using Templater.Service.Implementation;
+using TemplaterBonsai.Repository;
+using TemplaterBonsai.Repository.Implementation;
+using TemplaterBonsai.Repository.Implementation.V2;
 
 namespace SourceConsole
 {
     class MainClass
     {
+
+        //-init
+        //-view
+        //-call
+        //-return
+            //-viewname <>
         public static void Main(string[] args)
         {
+            IBonsaiStructureRepository MainRepo = null;
             var readerRepo = new ProjectReaderRepository(new FileService());
-			var MainRepo = new BonsaiFrameworkRepository(
-            //var MainRepo = new XamarinFormsScreenGeneratorRepository(
-                new FileService(), 
-                new SimpleCSharpProjectFactory(readerRepo),
-                readerRepo);
-            MainRepo.RunSteps();
+            var commandParameters = from arg in args
+                                    where arg.StartsWith('-')
+                                    select arg;
+            List<string> commandList = commandParameters.ToList();
+            var argument = "-viewname";
+            var functionParamaters = from arg in args
+                                     where !arg.StartsWith('-')
+                                     select new { argument, arg  };
+            var str = functionParamaters.ToDictionary(key => key.argument, value => value.arg);
+
+            foreach (var arg in commandParameters)
+            {
+                if (arg.Equals("-init", System.StringComparison.OrdinalIgnoreCase))
+                {
+                    MainRepo = new BonsaiFrameworkRepository(
+                    new FileService(), new SimpleCSharpProjectFactory(readerRepo), readerRepo);
+                    Console.WriteLine(str["-viewname"]);
+                }
+                if (arg.Equals("-view", System.StringComparison.OrdinalIgnoreCase))
+                {
+                    MainRepo = new BonsaiViewControlStructureRepository(
+                        new FileService(), new SimpleCSharpProjectFactory(readerRepo), readerRepo, str["-viewname"]);
+                }
+                if (arg.Equals("-call", System.StringComparison.OrdinalIgnoreCase))
+                {
+                    MainRepo = new BonsaiCallingStructureRepository(
+                        new FileService(), new SimpleCSharpProjectFactory(readerRepo), readerRepo, str["-viewname"]);
+                }
+                if (arg.Equals("-return", System.StringComparison.OrdinalIgnoreCase))
+                {
+                    MainRepo = new BonsaiReturnCallingStructureRepository(
+                        new FileService(), new SimpleCSharpProjectFactory(readerRepo), readerRepo, str["-viewname"]);
+                }
+            }
+            if (MainRepo != null)
+                MainRepo.RunSteps();
         }
     }
 }
