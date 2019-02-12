1110e628d8114f9caa6b7a09b8eea48c44f4a23c
Bianca Minnaar <bminnaar@gmail.com>
2019-02-12 12:17:45 +0200

    Upgrade SourceConsole to use GameLoop

--- Program.cs
+++ Program.cs
@@ -9,6 +9,31 @@ namespace SourceConsole
 {
     class MainClass
     {
+        private static bool GetConfirmation(out string[] args)
+        {
+            Console.Write("Enter Bonsai Command. <1> Init Bonsai <2> Create View\r\n<h>elp <q>uit :");
+            var command = Console.ReadLine();
+            if (command != "" && command.ToUpper() != "Q")
+            {
+                switch(command)
+                {
+                    case "1":
+                        args = "-init -view System".Split(" ");
+                        return true;
+                    case "2":
+                        Console.WriteLine("Please indicate your View name eg. Login :");
+                        var viewname = Console.ReadLine();
+                        args = ("-view " + viewname).Split(" ");
+                        return true;
+                    default:
+                        Console.WriteLine("Only use available Commans");
+                        return GetConfirmation(out args);
+                }
+            }
+            args = null;
+            return false;
+        }
+
         //free calls:
         //-init
         //-view
@@ -24,17 +49,20 @@ namespace SourceConsole
         // -partial
         public static void Main(string[] args)
         {
-            var map = new CommandParameterFunctionMapManager(
-            new ProjectReaderRepository(new FileService()), args.ToList()
-                //You need a licence to call the paid functions.
-                //Please email me @ bianca@bonsaisoft.co.za
-                //,"","");
-                , ConfigurationManager.AppSettings["LicenceKey"], ConfigurationManager.AppSettings["LicenceValue"]);
-            var commandMapKeys =map.GetCommandMap(args);
-            foreach(var command in commandMapKeys)
+            while(GetConfirmation(out args))
             {
-                Console.WriteLine(command.Value);
-                Console.ReadLine();
+                var map = new CommandParameterFunctionMapManager(
+                    new ProjectReaderRepository(new FileService()), args.ToList()
+                    //You need a licence to call the paid functions.
+                    //Please email me @ bianca@bonsaisoft.co.za
+                    //,"","");
+                    , ConfigurationManager.AppSettings["LicenceKey"], ConfigurationManager.AppSettings["LicenceValue"]);
+                var commandMapKeys =map.GetCommandMap(args);
+                foreach(var command in commandMapKeys)
+                {
+                    Console.WriteLine(command.Value);
+                    Console.ReadLine();
+                }
             }
         }
     }
