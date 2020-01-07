c4eba3b9c41c80b487376e3b9d9069b9e3db26bd
Bianca Minnaar <bminnaar@gmail.com>
2020-01-07 19:58:13 +0200

    Setup to receive course information.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using SourceConsole.Repository.Implementation;
 using Templater.Repository.Implementation;
 using Templater.Service.Implementation;
 using TemplaterBonsai.Factory;
@@ -19,6 +21,9 @@ namespace SourceConsole
                 "<5> ViewModel\r\n" +
                 "<6> Add Business Logic Unit\r\n" +
                 "<7> Add Serviced Repository Method Call \r\n" +
+                "<8> Add Control \r\n" +
+                "<9> AddPopup \r\n" +
+                "<10> Partial \r\n" +
                 "\r\n<h>elp <q>uit :");
             var command = Console.ReadLine();
             if (command != "" && command.ToUpper() != "Q")
@@ -29,9 +34,14 @@ namespace SourceConsole
                         args = "-init -view System".Split(" ");
                         return true;
                     case "2":
+                        Console.Write("Please past your json to file \n\r Hit enter to continue :");
+                        Console.ReadLine();
+                        var model2Json = InputReaderRepository.ReadInputJson();
                         Console.Write("Please indicate your View name eg. Login :");
                         var viewname = Console.ReadLine();
-                        args = ("-view " + viewname).Split(" ");
+                        var args2 = new List<string>($"-viewWithModel {viewname}".Split(" "));
+                        args2.Add(model2Json);
+                        args = args2.ToArray();
                         return true;
                     case "3":
                         Console.WriteLine("Please indicate your ControlName name eg. SearchTileView :");
@@ -50,9 +60,14 @@ namespace SourceConsole
                         args = ("-modelProperty " + modelPropertyName + " " + modelPropertyType).Split(" ");
                         return true;
                     case "5":
+                        Console.Write("Please past your json to file \n\r Hit enter to continue :");
+                        Console.ReadLine();
+                        var model1Json = InputReaderRepository.ReadInputJson();
                         Console.Write("Please indicate your ViewModel name eg. Details :");
                         var viewModelName = Console.ReadLine();
-                        args = ("-viewModel " + viewModelName).Split(" ");
+                        var args5 = new List<string>($"-viewModel {viewModelName}".Split(" "));
+                        args5.Add(model1Json);
+                        args = args5.ToArray();
                         return true;
                     case "6":
                         Console.Write("Please indicate your ViewController name eg. Dashboard :");
@@ -65,6 +80,37 @@ namespace SourceConsole
                         Console.Write("Please indicate your MethodName name eg. GetAllocations :");
                         var methodName = Console.ReadLine();
                         args = ("-viewControllerMethodCall " + repoName + " " + methodName).Split(" ");
+                        return true;
+                    case "8":
+                        Console.Write("Please past your json to file \n\r Hit enter to continue :");
+                        Console.ReadLine();
+                        var modelJson = InputReaderRepository.ReadInputJson();
+                        Console.Write("Please indicate your Control name :");
+                        var controlName = Console.ReadLine();
+                        var args8 = new List<string>($"-control {controlName}".Split(" "));
+                        args8.Add(modelJson);
+                        args = args8.ToArray();
+                        return true;
+                    case "9":
+                        Console.Write("Please past your json to file \n\r Hit enter to continue :");
+                        Console.ReadLine();
+                        var popupModelJson = InputReaderRepository.ReadInputJson();
+                        Console.Write("Please indicate your Control name :");
+                        var popupName = Console.ReadLine();
+                        var args1 = new List<string>($"-popup {popupName}".Split(" "));
+                        args1.Add(popupModelJson);
+                        args = args1.ToArray();
+                        return true;
+                    case "10":
+                        Console.Write("Template please? :");
+                        var template = Console.ReadLine();
+                        
+                        Console.Write("NameSpace please :");
+                        var NameSpace = Console.ReadLine();
+
+                        Console.Write("DataModel please :");
+                        var Datamodel = Console.ReadLine();
+                        args = $"-partial {template} {NameSpace} {Datamodel}".Split(" ");
                         return true;
                     default:
                         Console.WriteLine("Only use available Commans");
@@ -95,7 +141,7 @@ namespace SourceConsole
                 var map = new CommandParameterFunctionMapManager(
                     new ProjectReaderRepository(new FileService()), args.ToList()
                     //You need a licence to call the paid functions.
-                    //Please email me @ bianca@bonsaisoft.co.za
+                    //Please email me @ bminnaar@gmail.com
                     //,"","");
                     , ConfigurationManager.AppSettings["LicenceKey"], ConfigurationManager.AppSettings["LicenceValue"]);
                 var commandMapKeys =map.GetCommandMap(args);
---
+++ Repository/Implementation/InputReaderRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Templater.Service.Implementation;
+
+namespace SourceConsole.Repository.Implementation
+{
+    public class InputReaderRepository : IInputReaderRepository
+    {
+        public static string ReadInputJson()
+        {
+            var filer = new FileService();
+            return filer.ReadFromFile($"Data/Inputs.json");
+        }
+    }
+}
